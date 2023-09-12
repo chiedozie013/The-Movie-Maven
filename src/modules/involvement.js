cca208299b2ead61ae77ed91b8812ee65fd27376
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-09-12 21:20:55 +0100

    added the involvement.js file and include endpoint

---
+++ src/modules/involvement.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+const likesApi =
+  "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/BHkrUTs0Q4TVtczjuly5/likes";
+
+const postLikes = async (id) => {
+  const test = { item_id: id };
+  const response = await fetch(likesApi, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(test),
+  });
+  return response;
+};
+
+const getLikesCount = async () => {
+  const likes = await fetch(likesApi);
+  const response = await likes.json();
+  return response;
+};
+
+export { postLikes, getLikesCount };
