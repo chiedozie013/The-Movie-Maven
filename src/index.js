25614ff3392cac671ede67f678bf813f07eb14f8
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-09-12 21:21:13 +0100

    updated index,js with imports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import "./style.css";
 import header from "./modules/header";
+import { getMovies } from "./modules/movies.js";
+import { movieCounter } from "./modules/movieCounter.js";
+import { postLikes, getLikesCount } from "./modules/involvement.js";
 
 header();
+getMovies(movieCounter, postLikes, getLikesCount);
