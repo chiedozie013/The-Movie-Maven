f0e328c669f5329a4708db60c74f7514e7fa3be8
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-09-14 10:53:37 +0100

    updated the getlikecount function as requested

--- src/modules/involvement.js
+++ src/modules/involvement.js
@@ -1,16 +1,16 @@
 /* eslint-disable no-unused-vars */
-const likesApi = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/BHkrUTs0Q4TVtczjuly5/likes';
+const likesApi =
+  "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/BHkrUTs0Q4TVtczjuly5/likes";
 
 const postLikes = async (id) => {
   const test = { item_id: id };
   const response = await fetch(likesApi, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(test),
   });
-  return response;
 };
 
 const getLikesCount = async () => {
